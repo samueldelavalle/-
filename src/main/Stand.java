340dd1cff36fea479ee8876116f4e990a42fd0af
Samuel De Lavalle Velasquez <samueldelavalle@uninorte.edu.co>
2024-09-18 14:26:15 -0500

    Second commit

---
+++ src/main/Stand.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package main;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author samueldelavalle
+ */
+public class Stand {
+    private int id;
+    private float precio;
+    private ArrayList<Editorial> listeditorial = new ArrayList<>(); 
+    
+}
